924e62fb3ee1ab27d53ce43b0b43f603123d46cf
Marina BG <marlyakh@yandex.ru>
2019-06-13 12:47:35 +0300

    изменения в файлах коньроллерах cat_prod... и top_menu...

--- lesson_4/controllers/catalog_product_category_page.php
+++ lesson_4/controllers/catalog_product_category_page.php
@@ -1,6 +1,6 @@
 <?php
-require_once ('../config/config.php');
-require_once ("../general/general.php");
+include_once(__DIR__.'../config/config.php');
+include_once(__DIR__.'../general/general.php');
 
 
 $tableNameSql = str_replace("-", "_", ((explode(".", end(explode("/", $_SERVER["PHP_SELF"]))))[0]));
--- lesson_4/controllers/top_menu_header.php
+++ lesson_4/controllers/top_menu_header.php
@@ -1,22 +1,32 @@
 <?
+class TopMenu {
+    private $menuItems;
 
-$arrayTopMenuItems = createTopMenuItemsObj ($topMenuItems);
+    public function __construct() {
+        $this->menuItems = createTopMenuItemsObj ($topMenuItems);
+    }	
 
-function checkMenutopActivity($arrayTopMenuItems) {
-    for ($i=0; $i < count($arrayTopMenuItems); $i++) {  
-        if ($arrayTopMenuItems[$i]->name == 'каталог') {
-            $arrayTopMenuItems[$i]->active = 'active';
+    function checkMenutopActivity() {
+        for ($i=0; $i < count($this->menuItems); $i++) {  
+            if ($this->menuItems[$i]->name == 'каталог') {
+                $this->menuItems[$i]->active = 'active';
+            }
+        }
+    }
+
+    function createTopMenu() {
+        $this->checkMenutopActivity();
+
+        for ($i=0; $i < count($this->menuItems); $i++) {  
+            if ($this->menuItems[$i]->active == 'active') {
+                echo '<li class="menu_item active"><a href='.$this->menuItems[$i]->href.'>'.$this->menuItems[$i]->name.'</a></li>';
+            
+            } else {
+                echo '<li class="menu_item"><a href='.$this->menuItems[$i]->href.'>'.$this->menuItems[$i]->name.'</a></li>';
+            } 
         }
     }
 }
 
-function createTopMenu($arrayTopMenuItems) {
-    for ($i=0; $i < count($arrayTopMenuItems); $i++) {  
-        if ($arrayTopMenuItems[$i]->active == 'active') {
-            echo '<li class="menu_item active"><a href='.$arrayTopMenuItems[$i]->href.'>'.$arrayTopMenuItems[$i]->name.'</a></li>';
-        
-        } else {
-            echo '<li class="menu_item"><a href='.$arrayTopMenuItems[$i]->href.'>'.$arrayTopMenuItems[$i]->name.'</a></li>';
-        } 
-    }
-}
+$topMenu = new TopMenu();
+echo $topMenu->createTopMenu();
